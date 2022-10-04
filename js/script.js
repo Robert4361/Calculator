f4fb489ccdf6009c40a7833a6a40c8f1e9a7d79b
Robert4361 <robert4361@gmail.com>
2022-10-04 12:13:50 +0200

    Add multiple calculations and fix possible bugs

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const digits = document.querySelectorAll('.digit');
 const deletes = document.querySelector('#delete');
 const operators = document.querySelectorAll('.operator');
 const clear = document.querySelector('#clear');
+const equal = document.querySelector('.equals');
 
 function add(a, b) {
   return a + b;
@@ -14,10 +15,10 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
-  return a / b;
+  return Math.round(a / b * 100) / 100;
 }
 
-function operate(a, b, operator) {
+function operate(a, operator, b) {
   switch (operator) {
     case "+":
       return add(a, b);
@@ -29,12 +30,13 @@ function operate(a, b, operator) {
       return multiply(a, b);
       break;
     case "/":
+      if(b == 0) return "math error";
       return divide(a, b);
       break;
   }
 }
 
-for(let i = 0; i < digits.length - 1; i++) {
+for(let i = 0; i < digits.length; i++) {
   digits[i].addEventListener("click", function(){
     addNumber(digits[i]);
   });
@@ -46,17 +48,30 @@ for(let i = 0; i < operators.length; i++) {
 }
 deletes.addEventListener('click', deleteOne);
 clear.addEventListener('click', ()=> display.textContent='')
+equal.addEventListener("click", calculate);
 
 function addNumber(digit) {
+  if(display.textContent == "math error") display.textContent = '';
   display.textContent += digit.textContent;
 }
 function addOperator(operator) {
-  if(display.textContent.slice(-1) == ' ') return;
+  if(display.textContent.slice(-1) == ' ' || display.textContent == "math error") return;
+
+  const regex = /[\+\-\*\/]/;
+  if(regex.test(display.textContent)) calculate();
 
   display.textContent += ` ${operator.textContent} `;
 }
 
 function deleteOne() {
   if(display.textContent.slice(-1) == ' ') display.textContent = display.textContent.slice(0, -3);
+  else if (display.textContent == "math error") display.textContent = '';
   else display.textContent = display.textContent.slice(0, -1);
+}
+
+function calculate(){
+  const parts = display.textContent.trim().split(" ");
+  if(parts.length < 3) return;
+
+  display.textContent = operate(Number(parts[0]), parts[1], Number(parts[2]));
 }
